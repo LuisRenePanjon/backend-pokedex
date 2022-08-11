a260c6b7d151f599e33be0fd51b217afe7b46a8a
Luis René Panjón <luchorene97@gmail.com>
2022-08-11 14:39:12 -0500

    Config global prefix to "api/v3"

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v3/');
   await app.listen(3000);
+  return app;
 }
-bootstrap();
+bootstrap().then((r) =>
+  console.log(`Server is running on ${r.getHttpServer().address().port}`),
+);
